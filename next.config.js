e9638004c613bcb0258a7912bef5e6cae0973ad8
pinkmeraki <ayndriane.f@gmail.com>
2025-02-09 01:22:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    
-    formats:['image/webp']
-  },
-  optimizeFonts: false,
-  basePath: '/valentines'
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  output: "export",  // Enables static export
+  images: { unoptimized: true },  // Fixes images for GitHub Pages
+  basePath: "/valentines",  // Change to match your repo name
+  assetPrefix: "/valentines"
+};
+
+module.exports = nextConfig;
