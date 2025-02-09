ca70cdc1852684019d2eecdb4c8cc2e1f0229428
pinkmeraki <ayndriane.f@gmail.com>
2025-02-09 02:05:52 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,22 @@
-"use client"
+"use client";
 import { useState } from 'react';
 
 export default function Home() {
-  const [mailSrc, setMailSrc] = useState('mail_closed.png');
+  const [mailSrc, setMailSrc] = useState('/valentines/mail_closed.png');
   const [isNoClicked, setIsNoClicked] = useState(false);
   const [isYes, setIsYes] = useState(false);
 
-
   const handleMailClick = () => {
-    setMailSrc((prevSrc) => (prevSrc === 'mail_closed.png' ? 'mail_opened.png' : 'mail_closed.png'));
+    setMailSrc((prevSrc) => 
+      prevSrc === '/valentines/mail_closed.png' ? '/valentines/mail_opened.png' : '/valentines/mail_closed.png'
+    );
   };
 
   const handleNoClick = () => {
-    var x = Math.floor(Math.random()*300)+1;
-    var y = Math.floor(Math.random()*400)+1;
+    var x = Math.floor(Math.random() * 300) + 1;
+    var y = Math.floor(Math.random() * 400) + 1;
     setIsNoClicked(true);
-    
+
     const linkElement = document.querySelector('#no');
     linkElement.style.position = 'absolute';
     linkElement.style.right = `${x}px`;
@@ -24,67 +25,64 @@ export default function Home() {
 
   const handleYes = () => {
     setIsYes(true);
+  };
 
-  }
   return (
     <>
-    { !isYes ?
-      <>
-        <div className="mail">
-          <img 
-            src={mailSrc}
-            alt="Mail"
-            width={500}
-            onClick={handleMailClick}
-            className={mailSrc === 'mail_closed.png' ? 'mail-hover' : ''}
+      {!isYes ? (
+        <>
+          <div className="mail">
+            <img 
+              src={mailSrc}
+              alt="Mail"
+              width={500}
+              onClick={handleMailClick}
+              className={mailSrc === '/valentines/mail_closed.png' ? 'mail-hover' : ''}
             />
-          <h1 className='text-4xl sm:text-5xl'>
-            You got mail!
-          </h1>
-        </div>
-        <img
-          src="heart.png"
-          width={200}
-          className={`heart ${mailSrc === 'mail_opened.png' ? '' : 'hidden'}`}
-        />
+            <h1 className="text-4xl sm:text-5xl">
+              You got mail!
+            </h1>
+          </div>
+          <img
+            src="/valentines/heart.png"
+            width={200}
+            className={`heart ${mailSrc === '/valentines/mail_opened.png' ? '' : 'hidden'}`}
+          />
 
-        <div className={`form absolute max-w-[800px] text-md p-10 ${mailSrc === 'mail_opened.png' ? '' : 'hidden'}`}>
-          <h4>Dear, Rakiah</h4>
+          <div className={`form absolute max-w-[800px] text-md p-10 ${mailSrc === '/valentines/mail_opened.png' ? '' : 'hidden'}`}>
+            <h4>Dear, Rakiah</h4>
+            <br />
+            <h4>Will you be my valentine?</h4>
+            <br />
+            <h4>Love, Dria</h4>
+            <div className="yesno">
+              <a 
+                className="yes relative z-10"
+                onClick={handleYes}
+              >
+                Yes
+              </a>
+              <a
+                id="no"
+                className={`no ${isNoClicked ? 'absolute-position' : ''}`}
+                onClick={handleNoClick}
+              >
+                No
+              </a>
+              <a className={`${isNoClicked ? 'invisible' : 'hidden'}`}>
+                No
+              </a>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="text-center flex flex-col items-center">
+          <h4>Yay! See you this weekend!</h4>
           <br />
-          <h4>Will you be my valentine?</h4>
-          <br />
-          <h4>Love, Dria</h4>
-          <div className='yesno'>
-            <a 
-              className='yes relative z-10'
-              onClick={handleYes}
-            >
-              Yes
-            </a>
-            <a
-              id="no"
-              className={`no ${isNoClicked ? 'absolute-position' : ''}`}
-              onClick={handleNoClick}
-            >
-              No
-            </a>
-            <a
-              className={` ${isNoClicked ? 'invisible' : 'hidden'}`}
-            >
-              No
-            </a>
-              </div>
+          <img width={150} src="/valentines/vday1.gif" alt="Valentine's GIF"/>
+          <h5>I love you!</h5>
         </div>
-      </>
-      :
-      <div className='text-center flex flex-col items-center'>
-        <h4>Yay! See you this weekend!</h4>
-        <br />
-        <img width={150} src="vday1.gif"/>
-        <h5>I love you!</h5>
-
-      </div>
-      }
+      )}
     </>
   );
 }
